54aa09a62de5c5ea18dd7cbb6e44635e23fa8c27
Milton Siqueira <miltonsiqueira@gmail.com>
2016-04-03 16:06:33 +0100

    Split the main page into Shopping List and Select Product

---
+++ web-app/src/main/webapp/data.js
@@ -0,0 +1,238 @@
+var shoppingList = shoppingList || {};
+
+shoppingList.data = {
+  "products": [{
+    "name": "Red apples",
+    "prices": [
+      {
+        "value": 1.79,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Cream crackers - Tesco",
+    "prices": [
+      {
+        "value": 0.26,
+        "place": "Tesco"
+          }]
+      }, {
+    "name": "Organic Oats - Flahavan's",
+    "prices": [
+      {
+        "value": 2.67,
+        "place": "Tesco"
+          }]
+      }, {
+    "name": "Batata Palha Tradicional - Visconti",
+    "prices": [
+      {
+        "value": 2.50,
+        "place": "Mercado Brasileiro (near by SEDA)"
+          }]
+      }, {
+    "name": "Cork Flakes - Tesco",
+    "prices": [
+      {
+        "value": 1.49,
+        "place": "Tesco"
+          }]
+      }, {
+    "name": "Milk dairy free - Koko",
+    "prices": [
+      {
+        "value": 1.99,
+        "place": "Holland & Barrett"
+          }]
+      }, {
+    "name": "Water - Ballygowan",
+    "prices": [
+      {
+        "value": 1.00,
+        "place": "EuroGiant"
+          }]
+      }, {
+    "name": "Cotton Buds",
+    "prices": [
+      {
+        "value": 0.89,
+        "place": "Tesco"
+          }]
+      }, {
+    "name": "Bread Loaf Seed - Wholesome",
+    "prices": [
+      {
+        "value": 1.09,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Microwave popcorn - Spar",
+    "prices": [
+      {
+        "value": 1.50,
+        "place": "Spar"
+          }]
+      }, {
+    "name": "Organic Spaghetti - Tesco",
+    "prices": [
+      {
+        "value": 1.04,
+        "place": "Tesco"
+          }]
+      }, {
+    "name": "Microwave popcorn - Kelvin",
+    "prices": [
+      {
+        "value": 2.77,
+        "place": "Tesco"
+          }]
+      }, {
+    "name": "Cherry Tomato",
+    "prices": [
+      {
+        "value": 0.79,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Red grapes Punnet",
+    "prices": [
+      {
+        "value": 1.49,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Banana loose",
+    "prices": [
+      {
+        "value": 1.25,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Garlig Baguettes",
+    "prices": [
+      {
+        "value": 0.75,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Rocket Leaves - Meadow Fresh",
+    "prices": [
+      {
+        "value": 0.99,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Organic Smoked Salmon",
+    "prices": [
+      {
+        "value": 2.99,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Milled Linseed Sesame",
+    "prices": [
+      {
+        "value": 2.59,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Olive oil",
+    "prices": [
+      {
+        "value": 3.29,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Nutella",
+    "prices": [
+      {
+        "value": 2.69,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Filled tuna in brine",
+    "prices": [
+      {
+        "value": 1.29,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Sweet potatoes",
+    "prices": [
+      {
+        "value": 1.39,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Avocado",
+    "prices": [
+      {
+        "value": 2.69,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Chestnut mushroom",
+    "prices": [
+      {
+        "value": 0.89,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Carrots",
+    "prices": [
+      {
+        "value": 0.49,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Baby Leaf Spinach",
+    "prices": [
+      {
+        "value": 1.49,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Peppers Mixed",
+    "prices": [
+      {
+        "value": 1.59,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Greek Yorgut Strawberry",
+    "prices": [
+      {
+        "value": 1.49,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Gouda Cheese Slices",
+    "prices": [
+      {
+        "value": 1.49,
+        "place": "Lidl"
+          }]
+      }, {
+    "name": "Organic Spaghetti",
+    "prices": [
+      {
+        "value": 0.89,
+        "place": "Lidl"
+          }]
+      }, {
+
+    "name": "Strawberry",
+    "prices": [
+      {
+        "value": 2.99,
+        "place": "Lidl"
+          },
+      {
+        "value": 3.10,
+        "place": "Tesco"
+          },
+      {
+        "value": 1.00,
+        "place": "Street Market"
+          }]
+      }]
+}
